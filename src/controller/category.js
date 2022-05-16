5ee2849b68e605a24b6d99920ad5a59b26411e9e
wahyuwww <wahyu.aan2508@gmail.com>
2022-05-16 17:45:45 +0700

    filter category

--- src/controller/category.js
+++ src/controller/category.js
@@ -54,11 +54,21 @@ const categoryController = {
       const offset = (page - 1) * limit
       const type = req.query.type
       // console.log(type)
-      const result = await categoryModel.modelCategories.sortByCategori({ type, limit, offset })
-      console.log(result)
-      res.json({
-        data: result.rows
-      })
+      // const result = await categoryModel.modelCategories.sortByCategori({ type, limit, offset })
+      const {
+        rows: [count]
+      } = await categoryModel.modelCategories.countCategory()
+      const totalData = parseInt(count.total)
+      const totalPage = Math.ceil(totalData / limit)
+      // console.log(totalPage)
+      const pagination = {
+        currentPage: page,
+        limit,
+        totalData,
+        totalPage
+      }
+      const { rows } = await categoryModel.modelCategories.sortByCategori({ type, limit, offset })
+      commonHellper.response(res, rows, 'get data by filter', 200, pagination)
     } catch (error) {
       console.log(error)
       next(createError)
