2d442ea032679c0a1deeefc8ded9087e61ca82a3
wahyuwww <wahyu.aan2508@gmail.com>
2022-05-27 20:11:47 +0700

    product non token

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -68,7 +68,7 @@ const uploadImg = {
       } else {
         try {
           const image = req.files.map((file) => {
-            return 'http://localhost:4000/img/' + file.filename
+            return 'https://belanjain-aja.herokuapp.com/img' + file.filename
           })
           req.body.image = image.join(',')
         } catch {
